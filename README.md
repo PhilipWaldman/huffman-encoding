5cba347a62bc7e62a625e59f71f85aed0e6f6412
Philip Waldman <philip.waldman01@gmail.com>
2020-11-23 18:14:56 +0100

    Encoding works. Printing looks nice.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Huffman Encoding
+Creates a Huffman encoding for the given message.
+
+## What does the program do?
+1. Get the message to encode from the user. If no message is provided, `hot-diggity-dog` will be used.
+2. A frequency table is made with the frequencies of each character. This list is sorted in order of most to least common.
+3. The Huffman encoding tree is made. It works by...
+4. A code book is made from the tree. The code book is a dictionary with the character as a key and the bitstring to encode it into as the value. This makes encoding and decoding easier.
+5. The message is encoded using the code book. Every character is replaced with its corresponding encoding.
+6. The encoded message is decoded again using the code book.
+7. The entropy of the message is calculated. For the list of frequencies `F = f_1, ..., f_n`, the entropy is `H(F) = - \sum_{i=0}^{n} (f_i * log2(f_i))`. This is the theoretical average amount of information in the message.
+8. The actual average amount of information is calculated taking the sum of the length of the encoding of each character multiplied by its frequency.
+9. The compression ratio is calculated by dividing the length of the input message by the length of the encoded message.
--- huffman.py
+++ huffman.py
@@ -1,64 +1,49 @@
 import math
+from typing import Tuple, List, Dict, Any
 
 
 def main():
     message = get_message()
     freqs = sort_freqs(get_frequency(message))
     tree = make_tree(freqs)
-    codebook = make_codebook(tree)
+    code_book = make_code_book(tree)
+    encoded_message = encode_message(message, code_book)
+    decoded_message = decode_message(encoded_message, code_book)
     entropy = calc_entropy(freqs)
-    ave_info = calc_ave_info(freqs, codebook)
-    encoded_message = encode_message(message, codebook)
+    ave_info = calc_ave_info(freqs, code_book)
     compress_ratio = len(message) / len(encoded_message)
-    decoded_message = decode_message(encoded_message, codebook)
 
     print('\nMessage: {}'.format(message))
     print('Frequencies: {}'.format(printable_freqs(freqs)))
     print('Huffman tree: {}'.format(tree))
-    print('Codebook: {}'.format(codebook))
+    print('Code book: {}'.format(code_book))
+    print('Encoded message: {}'.format(encoded_message))
+    print('Decoded message: {}'.format(decoded_message))
     print('Entropy: {}'.format(round(entropy, 3)))
     print('Average info: {}'.format(round(ave_info, 3)))
-    print('Encoded message: {}'.format(encoded_message))
     print('Compression ratio: {}'.format(round(compress_ratio, 3)))
-    # print('Decoded message: {}'.format(decoded_message))
+    print_tree(tree)
 
 
-def get_message():
+def get_message() -> str:
+    """
+    Prompts the user to enter a message. if no message is entered 'hot-diggity-dog' is returned.
+
+    :return: The message
+    """
     message = input('Enter a message to be encoded: ')
     if len(message) == 0:
         return 'hot-diggity-dog'
     return message
 
 
-def make_tree(freqs):
-    tree = []
-    while len(freqs) > 1:
-        tree = [freqs[len(freqs) - 2][0], freqs[len(freqs) - 1][0]]
-        freq = freqs[len(freqs) - 2][1] + freqs[len(freqs) - 1][1]
-        freqs = freqs[:-2]
-        freqs.append((tree, freq))
-        freqs = sort_freqs(freqs)
+def get_frequency(message: str) -> List[Tuple[str, float]]:
+    """
+    Calculates the frequencies of every character in message.
 
-    return tree
-
-
-def print_tree(tree):
-    pass
-
-
-def sort_freqs(L):
-    for i in range(1, len(L)):
-        j = i - 1
-        key = L[i]
-        while (L[j][1] > key[1]) and (j >= 0):
-            L[j + 1] = L[j]
-            j -= 1
-        L[j + 1] = key
-    L.reverse()
-    return L
-
-
-def get_frequency(message: str):
+    :param message: The message to find the frequencies of.
+    :return: A list with the frequency of every character.
+    """
     freqs_dict = {}
     for let in message:
         if let in freqs_dict:
@@ -72,72 +57,190 @@ def get_frequency(message: str):
     return freqs
 
 
-def calc_entropy(freqs):
+def sort_freqs(freqs: List[Tuple[Any, float]]) -> List[Tuple[Any, float]]:
+    """
+    Sorts the character-frequency pairs in descending order of frequency.
+
+    :param freqs: The frequencies list to be sorted.
+    :return: The same list but sorted.
+    """
+    for i in range(1, len(freqs)):
+        j = i - 1
+        key = freqs[i]
+        while (freqs[j][1] > key[1]) and (j >= 0):
+            freqs[j + 1] = freqs[j]
+            j -= 1
+        freqs[j + 1] = key
+    freqs.reverse()
+    return freqs
+
+
+def make_tree(freqs: List[Tuple[Any, float]]) -> list:
+    """
+
+    :param freqs: The list with the frequency of every character to make an encoding off.
+    :return: A Huffman encoding tree made from the frequencies.
+    """
+    tree = []
+    while len(freqs) > 1:
+        tree = [freqs[len(freqs) - 2][0], freqs[len(freqs) - 1][0]]
+        freq = freqs[len(freqs) - 2][1] + freqs[len(freqs) - 1][1]
+        freqs = freqs[:-2]
+        freqs.append((tree, freq))
+        freqs = sort_freqs(freqs)
+    return tree
+
+
+def make_code_book(tree: list) -> Dict[str, str]:
+    """
+
+    :param tree: The Huffman encoding tree
+    :return:
+    """
+    code_book = {}
+
+    left_branch = tree[0]
+    if type(left_branch) is str:
+        code_book[left_branch] = '0'
+    else:
+        sub_tree = make_code_book(left_branch)
+        for key in sub_tree.keys():
+            sub_tree[key] = '0' + sub_tree[key]
+        code_book.update(sub_tree)
+
+    right_branch = tree[1]
+    if type(right_branch) is str:
+        code_book[right_branch] = '1'
+    else:
+        sub_tree = make_code_book(right_branch)
+        for key in sub_tree.keys():
+            sub_tree[key] = '1' + sub_tree[key]
+        code_book.update(sub_tree)
+
+    return code_book
+
+
+def calc_entropy(freqs: List[Tuple[Any, float]]) -> float:
+    """
+
+    :param freqs:
+    :return:
+    """
     entropy = 0
     for key, freq in freqs:
         entropy += freq * math.log2(freq)
     return -entropy
 
 
-def calc_ave_info(freqs, codebook):
+def calc_ave_info(freqs: List[Tuple[str, float]], code_book: Dict[str, str]) -> float:
+    """
+
+    :param freqs:
+    :param code_book:
+    :return:
+    """
     info = 0
     for key, freq in freqs:
-        info += freq * len(codebook[key])
+        info += freq * len(code_book[key])
     return info
 
 
-def make_codebook(tree):
-    codebook = {}
+def encode_message(message: str, code_book: Dict[str, str]) -> str:
+    """
 
-    left_branch = tree[0]
-    if type(left_branch) is str:
-        codebook[left_branch] = '0'
-    else:
-        sub_tree = make_codebook(left_branch)
-        for key in sub_tree.keys():
-            sub_tree[key] = '0' + sub_tree[key]
-        codebook.update(sub_tree)
-
-    right_branch = tree[1]
-    if type(right_branch) is str:
-        codebook[right_branch] = '1'
-    else:
-        sub_tree = make_codebook(right_branch)
-        for key in sub_tree.keys():
-            sub_tree[key] = '1' + sub_tree[key]
-        codebook.update(sub_tree)
-
-    return codebook
-
-
-def encode_message(message: str, codebook):
+    :param message:
+    :param code_book:
+    :return:
+    """
     encoded_message = ''
     for let in message:
-        encoded_message += codebook[let]
+        encoded_message += code_book[let]
     return encoded_message
 
 
-def decode_message(encoded_message: str, codebook):
-    reverse_codebook = flip_dict(codebook)
+def decode_message(encoded_message: str, code_book: Dict[str, str]) -> str:
+    """
+
+    :param encoded_message:
+    :param code_book:
+    :return:
+    """
+    reverse_code_book = flip_dict(code_book)
 
     decoded_message = ''
     tally = ''
     for i in encoded_message:
         tally += i
-        if tally in reverse_codebook:
-            decoded_message += reverse_codebook[tally]
+        if tally in reverse_code_book:
+            decoded_message += reverse_code_book[tally]
             tally = ''
     return decoded_message
 
 
-def flip_dict(dictionary):
-    flipped = {}
-    for key, val in dictionary.items():
-        flipped[val] = key
-    return flipped
+def flip_dict(dictionary: dict) -> dict:
+    """
+    Swaps the key and values in the dictionary.
+
+    :param dictionary: The dictionary to flip.
+    :return: A similar dictionary but with the keys and values swapped.
+    """
+    return {value: key for key, value in dictionary.items()}
 
 
-def printable_freqs(freqs):
+def print_tree(tree, layer=0):
+    """
+    Prints the tree in a more readable format.
+
+    :param layer: The current layer. Only used for the recursion when printing.
+    :param tree: The Huffman tree to print
+    """
+    if type(tree) != str:
+        depth_lines = '  |' * (layer + 1)
+
+        print('if 0:\n{}then '.format(depth_lines), end='')
+        print_tree(tree[0], layer + 1)
+
+        print('{}else '.format(depth_lines), end='')
+        print_tree(tree[1], layer + 1)
+    else:
+        print(tree)
+
+    if layer == 0:
+        depth = calc_depth(tree)
+        print('<{}>'.format('-' * (depth * 3 - 2)))
+        print('Tree depth: {}'.format(depth))
+
+
+def calc_depth(tree, depth=0, max_depth=0) -> int:
+    """
+    Calculates the depth of the tree.
+
+    :param tree: The tree to calculate the depth of.
+    :param depth: The current depth. Used for recursion.
+    :param max_depth: The max depth. Used to keep track during recursion.
+    :return: The depth of the tree.
+    """
+    if type(tree) != str:
+        d0 = calc_depth(tree[0], depth + 1)
+        if d0 > max_depth:
+            max_depth = d0
+
+        d1 = calc_depth(tree[1], depth + 1)
+        if d1 > max_depth:
+            max_depth = d1
+    else:
+        return depth
+    return max_depth
+
+
+def printable_freqs(freqs: List[Tuple[str, float]]) -> List[Tuple[str, float]]:
+    """
+    Rounds the frequencies to 5 decimal places.
+
+    :param freqs: The list of character-frequency tuples
+    where the frequencies may have many numbers after the decimal point.
+    :return: The same list but with the frequencies rounded to 5 decimal places.
+    """
     return [(key, round(freq, 5)) for key, freq in freqs]
 
 
